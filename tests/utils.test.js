04e35de9b6f5847bfc7cf3e8d6e132e4afd31d4b
tzsk <mailtokmahmed@gmail.com>
2022-08-19 13:32:02 +0530

    Find proper vite config file

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,8 +1,17 @@
-import { describe, expect, test } from 'vitest';
-import { getOptions } from '../lib/utils';
+import enquirer from 'enquirer';
+import { describe, expect, test, vi } from 'vitest';
+import { askForProjectType, getOptions } from '../lib/utils';
 
 describe('Utils', () => {
   test('It can get a list of templates', () => {
     expect(getOptions()).toHaveLength(4);
+  });
+
+  test('it has list of project types to ask for', async () => {
+    vi.spyOn(enquirer, 'prompt').mockImplementation((value) =>
+      Promise.resolve(value)
+    );
+    const result = await askForProjectType();
+    expect(result).toMatchSnapshot();
   });
 });
